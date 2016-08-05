342108af23e7dcb4d4120d77af244249095b99bc
Carlos Melcon <cmelcon@fvi.edu>
2016-08-04 22:43:59 -0400

    qwre

--- js/app.js
+++ js/app.js
@@ -1,2 +1,13 @@
 const $ = require('jquery');
-const mysql = require('mysql');
+
+$.ajax({
+  url:"http://localhost:9999/task",
+  success: function(res, txt, xhr){
+    res = JSON.parse(res);
+    console.log(res);
+    for(var i = 0; i < res.length; i++){
+      $("#new").append(res[i].description);
+    }
+
+  }
+})
